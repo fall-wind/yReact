0a160da7b75f391fe510180ab83f118ec0fcbae2
yt <1520406335@qq.com>
2018-01-09 21:37:51 +0800

    push code

--- example/helloWorld/index.js
+++ example/helloWorld/index.js
@@ -1,22 +1,45 @@
 import { y, mount, Component } from '../../index'
+import render from '../../src/render'
 const containerNode = document.getElementById('app')
 
 function App(props) {
     return (
-        <div>{props.name}</div>
+        <div style={{ color: 'red' }}>{props.name}</div>
     )
 }
 
 class TopApp extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: 'hello world',
+        }
+    }
+
+    componentWillMount(props) {
+        console.error(props, 'componentWillMount...')
+    }
+
+    componentWillUpdate(props){
+        console.info('componentWillUpdate:')
+    }
+
     render() {
         const { children } = this.props
+        const { name } = this.state
         return (
-            <div>
-                <App name="hello" />
+            <div onClick={(e) => {
+                this.setState({
+                    name: 'hello'
+                })
+            }}>
+                <App name={name} />
             </div>
         )
     }
 }
 
+render(<TopApp />, containerNode)
 // mount(<TopApp />, containerNode)
-mount(<App name="a hah" />, containerNode)
+// mount(<App name="a hah" />, containerNode)
